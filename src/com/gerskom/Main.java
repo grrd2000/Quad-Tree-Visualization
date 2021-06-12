74ecfa3ebb0d3f53c397400cdf7c652d164b31cd
Gerard Skomin <gerard.skomin@gmail.com>
2021-06-12 19:09:54 +0200

    Last changes, new inputs

--- src/com/gerskom/Main.java
+++ src/com/gerskom/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         try {
-            new MyFrame(new ImageData("src/lab_input.jpg"));
+            new MyFrame(new ImageData("src/xD_input.jpg"));
         }
         catch (Exception exc) {
             System.out.println("Interrupted: " + exc.getMessage());
